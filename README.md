d632a1d325f7c85ca7b52cfc6631f9aa2445a4c6
robq <robq@example.com>
2018-12-13 15:14:06 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Cross-sectional study Cystic Fibrosis Sputum Progression 
+These notebooks are the accompanying material for the [publication]().
+
+This repository contains:
+
+1. Example data;
+2. Example R jupyter notebook;
+3. Example qiime code for computation of jackknifed Emperor plots with confidence ellipsoids;
+3. Example outputs.
+
+
---
+++ qiime.sh
@@ -0,0 +1,6 @@
+export PATH="/home/rsilva/miniconda2/bin:$PATH"
+
+biom convert -i feat_table.txt -o feat_table.biom --to-hdf5 --table-type 'Metaboli
+te table'
+jackknifed_beta_diversity.py -i feat_table.biom -o bdiv_jk1000 -e 1000 -m mapping_file.txt -p params.txt
+compare_categories.py --method adonis -i bdiv_jk106/unrarefied_bdiv/bray_curtis_feat_table.txt -m mapping_file.txt -c clustid -o adonis_out -n 999
